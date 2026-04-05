a9858fa73febb64685c8b06fb0ef6bb6a39d2e25
Pixility, Inc. <pixielity@gmail.com>
2026-04-05 20:57:38 +0200

    🧹 chore: clean up stale docs and lockfile

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+/**
+ * @file Prettier Configuration
+ * @description Re-exports the shared Prettier configuration from @nesvel/prettier-config.
+ * @see https://prettier.io/docs/en/configuration#sharing-configurations
+ */
+
+export default '@nesvel/prettier-config';
